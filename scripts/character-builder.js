58dc166174a660f15a776f6226eeabf652452fa6
joaovitorcordeiro-dev <joaovitorscordeiro.contact@gmail.com>
2026-05-29 13:07:57 -0300

    Create findMatchingId function in character-builder.js

--- scripts/character-builder.js
+++ scripts/character-builder.js
@@ -1,15 +1,21 @@
 import { transferCharacterData, characterId, loadId } from "./data/storage.js";
+import { characterTraits } from "./data/races-data.js";
 
 loadId();
 
 const chosenId = characterId;
 
-const racesInfo = racesData;
+export function findMatchingId(racesInfo){
+  let matchingId;
 
-export function findMatchingId(racesInfo, characterId){
-  racesInfo.array.forEach(characterInfo => {
+  racesInfo.forEach(characterInfo => {
     if(chosenId === characterInfo.raceName){
-
+      matchingId = characterInfo;
     }
   });
+  return matchingId;
 }
+
+let chosenRace = findMatchingId(characterTraits);
+
+console.log(chosenRace);
