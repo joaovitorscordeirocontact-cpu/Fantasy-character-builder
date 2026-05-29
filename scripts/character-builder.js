1baf25be9536f8554b6b0698fbb08591c8c822db
joaovitorcordeiro-dev <joaovitorscordeiro.contact@gmail.com>
2026-05-29 11:16:45 -0300

    Change the name of function saveToStorage to saveId

---
+++ scripts/character-builder.js
@@ -0,0 +1,5 @@
+import { characterId } from "./index.js";
+
+
+
+console.log(characterId);
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let characterId = '';
+export let characterId = '';
 
 let characterTraits = [{
 raceName: '',
@@ -14,36 +14,48 @@ function transferCharacterData(){
   document.querySelector('.js-human-button')
     .addEventListener('click', () => {
       characterId = 'Human';
+      saveId();
+      window.location.href = 'character-builder.html';
     });
   
   document.querySelector('.js-elf-button')
     .addEventListener('click', () => {
       characterId = 'Elf';
+      saveId();
+      window.location.href = 'character-builder.html';
     });
 
   document.querySelector('.js-shadow-button')
     .addEventListener('click', () => {
       characterId = 'Shadow';
+      saveId();
+      window.location.href = 'character-builder.html';
     });
 
   document.querySelector('.js-orc-button')
     .addEventListener('click', () => {
       characterId = 'Orc';
+      saveId();
+      window.location.href = 'character-builder.html';
     });
 
   document.querySelector('.js-android-button')
     .addEventListener('click', () => {
       characterId = 'Android';
+      saveId();
+      window.location.href = 'character-builder.html';
     });
 
   document.querySelector('.js-beastman-button')
     .addEventListener('click', () => {
       characterId = 'Beastman';
+      saveId();
+      window.location.href = 'character-builder.html';
     });
-
-    savaToStorage();
 }
 
-function savaToStorage(){
+function saveId(){
   localStorage.setItem('characterId', JSON.stringify(characterId));
 }
+
+transferCharacterData();
