141d0dfbebf6cd5170c40c057cc3240aa25fc62f
joaovitorcordeiro-dev <joaovitorscordeiro.contact@gmail.com>
2026-06-02 15:10:04 -0300

    Create the presentation page

---
+++ scripts/presentation.js
@@ -0,0 +1,21 @@
+
+/*
+function renderPresentation(){
+  let imageName = chosenRace.raceName.toLowerCase();
+
+  let titleHTML = `Costumize your ${imageName} character.
+  `;
+
+  let characterHTML = `
+      <img class="character" src="./assets/images/character-sprites/${imageName}/${imageName}.png">
+  `;
+
+  document.querySelector('.js-title')
+    .textContent = titleHTML;  
+
+  document.querySelector('.js-character-space')
+    .innerHTML = characterHTML;
+}
+
+renderPresentation();
+*/
